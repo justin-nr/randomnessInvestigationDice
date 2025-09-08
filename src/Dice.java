f72cffeda36a3b77ec2fe8d582e9b4f2b92b8774
justin-nr <justin98025@gmail.com>
2025-09-07 18:15:07 -0700

    -added an array for storage -added for loops to use the die rolls from the storage array -removed previous for loop that didnt use array

--- src/Dice.java
+++ src/Dice.java
@@ -12,24 +12,35 @@ public class Dice {
 
         //asks user how many die to roll
         System.out.println("How many die do you want to roll?");
-        //takes the input
+        //takes the input from user
         int dieAmount = myObj.nextInt();
         //asks user how many sides for the die
         System.out.println("How many sides do you want each to have?");
-        //takes the input
+        //takes the input from user
         int dieSides = myObj.nextInt();
 
-        //output for input by user
-        System.out.println("You will roll " + dieAmount + " die" + " with " + dieSides + " sides!");
-        System.out.println("In total you rolled a ")
+        //output in console for input by user
+        System.out.println("You will roll " + dieAmount + " die" + " with each having " + dieSides + " sides!");
 
-        //for loop for each die roll
-        for (int i = 1; i <= dieAmount; i++) {
-            int roll = rand.nextInt(dieSides) + 1;
-            //writes results in console
-            System.out.println("Die " + i + " rolled:" + roll);
+        //array for results(storage)
+        int[] results = new int[dieAmount];
+        //creates "total" integer
+        int total = 0;
+
+        //for loop to roll each die and then store the results in an array
+        for (int i = 0; i < dieAmount; i++) {
+            //rolls the die
+            results[i] = rand.nextInt(dieSides) + 1;
+            //adds the total
+            total += results[i];
         }
 
+        //for loop to print out the die rolls from the array
+        for (int i = 0; i < dieAmount; i++) {
+            System.out.println("Die " + (i + 1) + " rolled a " + results[i]);
+        }
+        //prints out results
+        System.out.println("In total you rolled a " + total + "!");
 
     }
 }
