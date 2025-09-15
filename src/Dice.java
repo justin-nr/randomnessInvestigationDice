5229c20a6f0e397dc0e0a921c37cf582d3d3f5f7
justin-nr <justin98025@gmail.com>
2025-09-14 19:03:18 -0700

    -added number validation for user input

--- src/Dice.java
+++ src/Dice.java
@@ -1,26 +1,54 @@
 //imports random class
+
 import java.util.Random;
 //imports scanner class
 import java.util.Scanner;
 
 public class Dice {
     public static void main(String[] args) {
+
         //creates random obj
         Random rand = new Random();
         //creates scanner obj
         Scanner myObj = new Scanner(System.in);
 
-        //asks user how many die to roll
-        System.out.println("How many die do you want to roll?");
-        //takes the input from user
-        int dieAmount = myObj.nextInt();
-        //asks user how many sides for the die
-        System.out.println("How many sides do you want each to have?");
-        //takes the input from user
-        int dieSides = myObj.nextInt();
+        //initializes amount and sides
+        int dieAmount;
+        int dieSides;
 
-        //output in console for input by user
-        System.out.println("You will roll " + dieAmount + " die" + " with each having " + dieSides + " sides!");
+        //prompt for a valid number (1-20)
+        while (true) {
+            System.out.println("How many dice do you want to roll? (1-20)");
+            //makes input dieAmount
+            if(myObj.hasNextInt()){
+                dieAmount = myObj.nextInt();
+                //creates number restrictions
+                if (dieAmount >=1 && dieAmount <=20){
+                    break;
+                }
+            }else {
+                //throws away invalid input
+                myObj.next();
+            }
+            System.out.println("Please enter a valid number between 1 and 20");
+        }
+
+        while(true){
+            System.out.println("How many sides do you want each die to have? (1-6)");
+            //makes input dieSides
+            if (myObj.hasNextInt()){
+                dieSides = myObj.nextInt();
+                //creates number restrictions
+                if(dieSides >=1 && dieSides <=6){
+                    break;
+                }
+            }else{
+                //throws away invalid input
+                myObj.next();
+            }
+            System.out.println("Please input a valid number between 1 and 6.");
+        }
+
 
         //array for results(storage)
         int[] results = new int[dieAmount];
